e83783efb3de05bf2773a38445f33bac002a5fab
Bloodyaugust <greysonrichey@gmail.com>
2019-07-28 14:41:36 -0600

    Use un-optimized get_simple_path

--- scripts/path-navigator.gd
+++ scripts/path-navigator.gd
@@ -3,7 +3,7 @@ extends Navigation2D
 onready var end: Node2D = $"end_point"
 onready var spawn: Node2D = $"spawn_point"
 
-onready var path: PoolVector2Array = get_simple_path(spawn.position, end.position)
+onready var path: PoolVector2Array = get_simple_path(spawn.position, end.position, false)
 
 func _ready():
   var global_path: Array = []
