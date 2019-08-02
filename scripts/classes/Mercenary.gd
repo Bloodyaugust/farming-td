6c3d63e32a5c7d6400b868c4b0fdd4c52198ae22
Greyson Richey <greysonrichey@gmail.com>
2019-08-02 03:07:22 -0600

    Finish basic implementation of Mercenary class
    - Also finished the implementation of basic_mercenary
    - Other tweaks and bugfixes around enemies, attacking

--- scripts/classes/Mercenary.gd
+++ scripts/classes/Mercenary.gd
@@ -3,5 +3,39 @@ class_name Mercenary
 
 onready var navigation: Navigation2D = $"../mercenary_navigation"
 
+export var attack_damage: float
+export var attack_interval: float
+export var attack_range: float
+
+var attack_cooldown: float = 0
+var target: Node2D
+
 func _ready():
   print(navigation)
+  
+func _process(delta):
+  if attack_cooldown > 0:
+    attack_cooldown -= delta
+    if attack_cooldown < 0:
+      attack_cooldown = 0
+  
+  if is_instance_valid(target):
+    if position.distance_to(target.position) >= attack_range:
+      target = null
+  else:
+    find_target()
+    
+  if is_instance_valid(target) && attack_cooldown == 0:
+    attack_target()
+  
+func find_target():
+  var enemies = get_tree().get_nodes_in_group("enemies")
+  
+  for enemy in enemies:
+    if position.distance_to(enemy.position) <= attack_range:
+      target = enemy
+      break
+  
+func attack_target():
+  target.get_node("actor_health").damage(attack_damage)
+  attack_cooldown = attack_interval
