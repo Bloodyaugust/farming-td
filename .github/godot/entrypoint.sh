5a280ab511b34bdb30f4416b0bd63adeaa76704c
Greyson Richey <greysonrichey@gmail.com>
2019-08-08 02:38:17 -0600

    Fix local paths in entrypoint

--- .github/godot/entrypoint.sh
+++ .github/godot/entrypoint.sh
@@ -7,10 +7,12 @@ which butler
 godot --version
 butler -V
 
-godot --export "Windows Desktop" ./build/win/farming-td.exe
+mkdir build/win/
 
-ls -al ./
+godot --export "Windows Desktop" build/win/farming-td.exe
+
+ls -al
 
 butler login
 
-butler push ./build/win/ synsugarstudio/farming-td:win-alpha
+butler push build/win/ synsugarstudio/farming-td:win-alpha
