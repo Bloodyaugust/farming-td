a0d1ab59f6e000f91a53e94f6057a0f0459bfcd4
Bloodyaugust <greysonrichey@gmail.com>
2019-08-23 17:08:42 -0600

    Add build-and-deploy script

---
+++ tools/build-and-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# set -e
+
+which butler
+
+godot --version
+butler -V
+
+mkdir build/
+mkdir build/win/
+
+godot --export "Windows Desktop" build/win/farming-td.exe
+
+ls -al
+ls -al build/
+ls -al build/win/
+
+butler login
+
+butler push build/win/ synsugarstudio/farming-td:win-alpha
