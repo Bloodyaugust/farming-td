0c15db7738ae9a0c3cea97def2f24744c823200f
Bloodyaugust <greysonrichey@gmail.com>
2019-08-23 18:16:05 -0600

    MAKE THE BUILD FINALLY WORK WITH CCI YEEEEEEt

--- tools/build-and-deploy.sh
+++ tools/build-and-deploy.sh
@@ -4,18 +4,23 @@
 
 which butler
 
+cat ~/.local/share/godot/templates/3.1.1.stable/version.txt
 godot --version
 butler -V
 
 mkdir build/
+mkdir build/osx/
 mkdir build/win/
 
+godot --export "Mac OSX" build/osx/farming-td.zip -v
 godot --export "Windows Desktop" build/win/farming-td.exe -v
 
 ls -al
 ls -al build/
+ls -al build/osx/
 ls -al build/win/
 
 butler login
 
+butler push build/osx/ synsugarstudio/farming-td:osx-alpha
 butler push build/win/ synsugarstudio/farming-td:win-alpha
