969e7035554d8d3ec7311dd8ed26fc7eadf07a96
Greyson Richey <greysonrichey@gmail.com>
2019-08-27 01:53:21 -0600

    Make LevelLoader actually load and use waves
    - Tweak structure for level json
    - Set up for LevelLoader to call spawn event

--- scripts/classes/LevelLoader.gd
+++ scripts/classes/LevelLoader.gd
@@ -1,28 +1,64 @@
 extends Node
 class_name LevelLoader
 
-onready var parent = $"../" 
+onready var current_wave_index = 0
+onready var parent = $"../"
 
-onready var level_json = _load_level().result
+onready var level_json = _load_level()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# [{
+#   actors: [{
+#     amount: int,
+#     timer: int,
+#     type: string
+#   }],
+# }]
+onready var waves = level_json['waves']
 
-# Called when the node enters the scene tree for the first time.
+var current_wave = {}
+var wave_timers = {}
+
 func _ready():
-  print("res://data/" + parent.name + ".json")
-  print(level_json)
+  _initialize_wave()
+
+func _process(delta):
+  if current_wave_index < waves.size():
+    for i in range(0, current_wave['actors'].size()):
+      var current_actor = current_wave['actors'][i]
+
+      if current_actor['amount'] > 0:
+        wave_timers[i] -= delta
+
+        if wave_timers[i] <= 0:
+          current_actor['amount'] -= 1
+          wave_timers[i] = current_actor['timer']
+
+    if _is_actors_exhausted():
+      current_wave_index += 1
+      _initialize_wave()
+
+func _is_actors_exhausted():
+  var exhausted = true
+
+  # Are all the enemies dead?
+
+  # return exhausted
+  return false
+
+func _initialize_wave():
+  print("Initializing wave: " + str(current_wave_index))
+  current_wave = waves[current_wave_index]
+
+  for i in range(0, current_wave['actors'].size()):
+    wave_timers[i] = current_wave['actors'][i]['timer']
 
 func _load_level():
   var file = File.new()
   file.open("res://data/" + parent.name + ".json", File.READ)
-  
+
   var json = file.get_as_text()
-  print(json)
   file.close()
-  
+
   var parsed_json = JSON.parse(json)
-  print(parsed_json)
-  
+
   return parsed_json.result
