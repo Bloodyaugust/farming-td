9e6dda263581806ff3456ab816944474f14ce308
Bloodyaugust <greysonrichey@gmail.com>
2019-08-31 18:02:47 -0600

    Add basic experience gain and level up mechanics to Mercenary
    - Currently only affects a ramp for attack interval
    - Set up to be used for base stats, which will affect things like range, attack speed, need for food, etc

--- scripts/classes/Mercenary.gd
+++ scripts/classes/Mercenary.gd
@@ -9,34 +9,42 @@ onready var player_tilemap: TileMap = navigation.find_node("PlayerBaseLayerTilem
 onready var tile: Vector2 = player_tilemap.world_to_map(position)
 
 export var agility: int
-export var attack_damage: float
-export var attack_interval: float
+export var attack_damage: int
+export var attack_cooldown_min: float
+export var attack_cooldown_max: float
 export var attack_range: float
-export var experience: float
-export var level: int
-export var stamina: float
+export var experience: int
+export var experience_thresholds: PoolIntArray
+export var stamina: int
 export var strength: int
 
-var attack_cooldown: float = 0
+var attack_cooldown: float
+var time_to_attack: float = 0
 var indicator_base_size: float = 8
 var target: WeakRef
+var level: int = 0
+var level_modifier: float = 0
+var level_max: int
 
 func _ready():
+  attack_cooldown = attack_cooldown_max
+  level_max = experience_thresholds.size()
+  
   range_indicator.scale = range_indicator.scale * (attack_range / indicator_base_size)
   store.subscribe(self, "on_store_change")
 
 func _process(delta):
-  if attack_cooldown > 0:
-    attack_cooldown -= delta
-    if attack_cooldown < 0:
-      attack_cooldown = 0
+  if time_to_attack > 0:
+    time_to_attack -= delta
+    if time_to_attack < 0:
+      time_to_attack = 0
 
   if target != null and (!target.get_ref() or target.get_class() != "KinematicBody2D" or position.distance_to(target.position) >= attack_range):
     target = null
   else:
     find_target()
 
-  if target != null and attack_cooldown == 0:
+  if target != null and time_to_attack == 0:
     attack_target()
 
 func on_store_change(type, state):
@@ -56,5 +64,21 @@ func find_target():
       break
 
 func attack_target():
-    target.get_ref().get_node("actor_health").damage(attack_damage)
-    attack_cooldown = attack_interval
+  var target_health = target.get_ref().get_node("actor_health")
+  
+  if target_health.health > 0:
+    target_health.damage(attack_damage)
+    time_to_attack = attack_cooldown
+    # TODO: Grab XP from the enemy killed
+    if level != level_max and target_health.health <= 0:
+      experience += 1
+    
+      if experience >= experience_thresholds[level]:
+        level_up()
+
+func level_up():
+  experience = experience - experience_thresholds[level]
+  level += 1
+  level_modifier = level / level_max
+  
+  attack_cooldown = lerp(attack_cooldown_max, attack_cooldown_min, level_modifier)
